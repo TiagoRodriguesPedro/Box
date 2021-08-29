6bc98171eb8240bb796402488119ef505a4defff
Tiago Rodrigues <isatiago098@gmail.com>
2021-08-29 03:39:23 -0300

    Resoluçao exerçicio box

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+_____________+");
+            Console.WriteLine("|    TIAGO    |");
+            Console.WriteLine("+_____________+");
+        }
+    }
+}
